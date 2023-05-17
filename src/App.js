aae8b07eefbecb0817a72a89598eecce0e6ffb42
Abenezer Chane <abenezerchane@gmail.com>
2023-05-17 15:45:36 -0700

    Improvements 3,4,5

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
+
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
@@ -20,14 +21,23 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = "O";
     }
-    onPlay(nextSquares);
+    onPlay(nextSquares,i);
   }
-  const winner = calculateWinner(squares);
+  const object = calculateWinner(squares);
   let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
+  if (object) {
+    status = "Winner: " + object.winner;
+    highlight(object.pos[0],object.pos[1],object.pos[2]);
+  } 
+  else if (squares.every((square) => square !== null)) { 
+    status = "It's a Draw";
+  }
+  else {
     status = "Next player: " + (xIsNext ? "X" : "O");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.style.backgroundColor = "white";
+    });
   }
   const boarded = Array(3).fill(null).map((_, i) => {
     return (
@@ -55,22 +65,32 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
  
   const [history, setHistory] = useState([Array(9).fill(null)]); // [squares, squares, squares, ...
+  const [indexes, setIndexes] = useState([Array(2).fill(null)]); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9
   const [currentMove, setCurrentMove] = useState(0); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ...
   const xIsNext = currentMove % 2 === 0;
   const [ascending, setAscending] = useState(true);
   const currentSquares = history[currentMove];
+  
+  //const [winnerPos, setWinnerPos] = useState([]);
   //cons [movees , setMovees] = useState([])
   //const [squares, setSquares] = useState(Array(9).fill(null));
+  
 
-  function handlePlay(nextSquares) {
+  function handlePlay(nextSquares,i) {
     const nextHistory = [...history.slice(0, currentMove + 1),nextSquares];
+    const nextIndexes = [...indexes.slice(0, currentMove + 1),i];
+
     setHistory(nextHistory);
+    setIndexes(nextIndexes);
     setCurrentMove(nextHistory.length - 1);
+    
+    
   }
   function jumpTo(nextMove) {
     // TODO
     setCurrentMove(nextMove);
     setHistory(history.slice(0, nextMove + 1));
+    setIndexes(indexes.slice(0, nextMove + 1));
     
   }
   function sortMoves() {
@@ -79,20 +99,22 @@ export default function Game() {
     // moves.reverse();
     // console.log(moves);
     setAscending(!ascending);
-    console.log(typeof(moves));
+    
     
   }
   let sortDescription = ascending ? "Ascending" : "Descending";
   
   const moves = history.map((squares, move) => {
     let description;
+    let index = indexes[move];
+   
     if (move > 0) {
-      description = "Go to move #" + move;
+      description = "Go to move #" + move + " (" + Math.floor(index / 3) + ", " + index % 3 + ")";
     } else {
       description = "Go to game start";
     }
     if (move === currentMove) {
-      description =  "You are at move #" + move;
+      description =  "You are at move #" + move ;
       return  (
         <li key={move}>
           {description}
@@ -106,6 +128,9 @@ export default function Game() {
       </li>
     );
   });
+  if (!ascending) { 
+    moves.reverse();
+  };
 
   return (
     <div className="game">
@@ -120,6 +145,12 @@ export default function Game() {
       </div>
     </div>
   );
+}
+function highlight(i,j,k) {
+  const squares = document.querySelectorAll(".square");
+  squares[i].style.backgroundColor = "yellow";
+  squares[j].style.backgroundColor = "yellow";
+  squares[k].style.backgroundColor = "yellow";
 }
 
 function calculateWinner(squares) {
@@ -136,7 +167,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {winner : squares[a] , pos : [a,b,c]};
     }
   }
   return null;
